284cd5a000da2f76793c9591acb5dc14146ebd15
Wannes Salomé <mail@wannessalome.nl>
2026-03-22 09:25:14 +0100

    fix: expose SANITY_STUDIO_ env vars to vite dev server

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url));
 export default defineConfig({
   root: __dirname,
   plugins: [react()],
+  envPrefix: 'SANITY_STUDIO_',
   server: {
     port: 3333,
   },
